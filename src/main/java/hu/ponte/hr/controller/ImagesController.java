89621694e66e14714e1572b17355d728f602b40c
AzureAD\CsontosTamás <csonti515@gmail.com>
2021-10-06 09:20:40 +0200

    clean unused imports

--- src/main/java/hu/ponte/hr/controller/ImagesController.java
+++ src/main/java/hu/ponte/hr/controller/ImagesController.java
@@ -1,13 +1,11 @@
 package hu.ponte.hr.controller;
 
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import hu.ponte.hr.repository.ImageMetaJpaRepository;
+
 import hu.ponte.hr.services.ImageMetaStore;
 import hu.ponte.hr.services.ImageStore;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
